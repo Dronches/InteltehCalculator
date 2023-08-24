78477d085d0ed16f828ec444a15c1caff04b2dde
Andrey Petrov <theprivetandreybest@yandex.ru>
2023-08-24 15:12:29 +0300

    Add save geometry for main window

--- InteltehCalculator/mainwindow.cpp
+++ InteltehCalculator/mainwindow.cpp
@@ -13,3 +13,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    // Сохранение настроек геометрии
+    QSettings settings("Intelteh", "InteltehCalculator");
+    settings.setValue("MainWindow/geometry", saveGeometry());
+
+    // Выполнение базового функционала
+    QMainWindow::closeEvent(event);
+}
+
+void MainWindow::readSettings()
+{
+    // Извлечение и применение настроек геометрии
+    QSettings settings("Intelteh", "InteltehCalculator");
+    restoreGeometry(settings.value("MainWindow/geometry").toByteArray());
+}
--- InteltehCalculator/mainwindow.h
+++ InteltehCalculator/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QSettings>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    /*!
+     * \brief Обработка события закрытия окна
+     * \param event Событие закрытия окна
+     */
+    void closeEvent(QCloseEvent *event) override;
+
+    /*!
+     * \brief Считать сохранённые настройки
+     */
+    void readSettings();
+
 private:
+    /// Отображение главного окна
     Ui::MainWindow *ui;
+
+    /// Путь к геометрии главного окна
+    QString settings_mainWindow_Geometry = "MainWindow/geometry";
+
+    /// Путь к положению главного окна
+    QString settings_mainWindow_State = "MainWindow/state";
+
 };
 #endif // MAINWINDOW_H
