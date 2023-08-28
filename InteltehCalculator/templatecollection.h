7cfe7cfce6c73034b2a4649cadd2e79cd29fa8ee
Andrey Petrov <theprivetandreybest@yandex.ru>
2023-08-28 15:59:30 +0300

    Add fuctions realisation to header folder

--- InteltehCalculator/templatecollection.h
+++ InteltehCalculator/templatecollection.h
@@ -37,4 +37,42 @@ public:
     T* PopFront();
 };
 
+template <class T>
+TemplateCollection<T>::TemplateCollection()
+{
+
+}
+
+template <class T>
+TemplateCollection<T>::~TemplateCollection()
+{
+    // безопасное удаление всех элементов в очереди
+    while(GetSize()>0)
+    {
+        delete queueData.front();
+        queueData.pop();
+    }
+}
+
+template <class T>
+int TemplateCollection<T>::GetSize()
+{
+    return queueData.size();
+}
+
+template <class T>
+void TemplateCollection<T>::PushBack(T* element)
+{
+    queueData.push(element);
+}
+
+template <class T>
+T* TemplateCollection<T>::PopFront()
+{
+    T* tempData = queueData.front();
+    queueData.pop();
+    return tempData;
+}
+
+
 #endif // TEMPLATECOLLECTION_H
