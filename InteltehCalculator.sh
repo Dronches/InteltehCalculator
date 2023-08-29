3049da40c83a22c1454881407d6b238441709bcb
Andrey Petrov <theprivetandreybest@yandex.ru>
2023-08-29 13:45:50 +0300

    Add chmod ugo+x for all users

--- InteltehCalculator.sh
+++ InteltehCalculator.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:build-CalculatorOperationsLib-Desktop_Qt_5_12_12_GCC_64bit-Release
 export LD_LIBRARY_PATH
+chmod ugo+x build-InteltehCalculator-Desktop_Qt_5_12_12_GCC_64bit-Release/InteltehCalculator
+echo "Приложение калькулятор запускается..."
 ./build-InteltehCalculator-Desktop_Qt_5_12_12_GCC_64bit-Release/InteltehCalculator
